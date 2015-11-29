c10375b9c977212a6b01ec7b1254752414ce071f
Dan Kahle <Daniel.Kahle@acxiom.com>
2015-11-29 15:50:00 -0600

    latest

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,11 @@
 import {bootstrap, Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
-import {Hero} from './hero';
-
-var _heroes = [
-   {id: 1, name: 'dank'},
-   {id: 2, name: 'carl'}
-];
+import {HeroFormComponent} from './forms/hero-form.component';
 
 @Component({
    selector: 'my-app',
-   styleUrls: ['app/app.css'],
    templateUrl: 'app/app.html',
-   directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
+   directives: [CORE_DIRECTIVES,FORM_DIRECTIVES, HeroFormComponent]
 })
 class AppComponent {
-   title = 'my title';
-   heroes = _heroes;
-
-   addHero(newHero) {
-      this.heroes.push({id: this.heroes.length, name: newHero.value});
-      newHero.value = null;
-   }
-
 }
 bootstrap(AppComponent);
---
+++ src/app/forms/app.ts
@@ -0,0 +1,16 @@
+/*
+import {bootstrap, Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+import {HeroFormComponent} from './forms/hero-form.component'
+var _heroes = [
+   {id: 1, name: 'dank'},
+   {id: 2, name: 'carl'}
+];
+
+@Component({
+   selector: 'my-app',
+   templateUrl: 'app/app.html',
+   directives: [HeroFormComponent],
+})
+class AppComponent {}
+bootstrap(AppComponent);
+*/
