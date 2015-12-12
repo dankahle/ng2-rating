18f06e91f0bc61198ddc7297ce47a1f2a2c35eac
Dan Kahle <Daniel.Kahle@acxiom.com>
2015-12-12 13:39:31 -0600

    initial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import {bootstrap, Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
-import {HeroFormComponent} from './heroForm/hero-form.component';
 
 @Component({
    selector: 'my-app',
    templateUrl: 'app/app.html',
-   directives: [CORE_DIRECTIVES,FORM_DIRECTIVES, HeroFormComponent]
+   directives: [CORE_DIRECTIVES,FORM_DIRECTIVES]
 })
 class AppComponent {
+   name:string = 'dank';
 }
 bootstrap(AppComponent);
